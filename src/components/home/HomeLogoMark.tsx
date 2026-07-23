97186fb3d63c11145c0b05a7f6172a83cfe05143
이상한 <jdama3125@gmail.com>
2026-07-23 22:08:07 +0900

    fix: 푸른색 아파트 일러스트 로고 적용

--- src/components/home/HomeLogoMark.tsx
+++ src/components/home/HomeLogoMark.tsx
@@ -1,20 +1,12 @@
+import apartmentLogo from '../../assets/apartment-logo-blue.png'
+
 export default function HomeLogoMark() {
   return (
-    <svg
+    <img
       className="home-logo-mark"
-      viewBox="0 0 64 64"
-      role="img"
-      aria-label="보라매롯데낙천대 로고"
-    >
-      <circle cx="32" cy="32" r="30" fill="#ed1b2f" />
-      <path
-        fill="#fff"
-        d="M17 14.5v34.2c2.2-2.6 4.5-4.8 7-6.6V11.4a21 21 0 0 0-7 3.1Zm10-4.6v30.4c2.1-1 4.3-1.7 6.7-2.2V8.2c-.6-.1-1.2-.1-1.8-.1-1.7 0-3.3.2-4.9.5v1.3Zm9.7-1.1v28.8c2.3-.1 4.6.1 7 .6V13a23.7 23.7 0 0 0-7-4.2Z"
-      />
-      <path
-        fill="#fff"
-        d="M14.1 51.1c8.7-11.4 19.3-14.8 34-8.9-10.9-2-20.3.5-28.2 8.8 10.5-5.6 20.1-6.2 29.8-1.8-10.4 7.8-24.9 8.6-35.6 1.9Z"
-      />
-    </svg>
+      src={apartmentLogo}
+      alt=""
+      aria-hidden="true"
+    />
   )
 }
